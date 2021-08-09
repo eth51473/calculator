460dc5c01dd25c915d6263e71823761067e11c58
Ethan Bird <eth51473@gmail.com>
2021-08-09 14:05:18 -0600

    Further-Study

--- calc.js
+++ calc.js
@@ -27,8 +27,22 @@ reader.question("What would you like to calculate?", function(input){
 		console.log(num1 / num2)
 	}
 	if (mathSymbol == 'sqrt'){
-		console.log(Math.sqrt(num1 + num2))
+		console.log("the square root of your two numbers added together is: " + Math.sqrt(num1 + num2))
 	}
+	if (mathSymbol == '^2'){
+		console.log("the square of your two numbers added together is: "+ Math.pow((num1 + num2), 2))
+	}
+	if (mathSymbol == '^3'){
+		console.log("the cube of your two numbers added together is: "+ Math.pow((num1 + num2), 3))
+	}
+	if (mathSymbol == 'pwr'){
+		console.log(`${num1} to the power of ${num2} is : `+ Math.pow(num1, num2))
+	}
+	if (mathSymbol == 'mod'){
+		console.log(num1 % num2)
+	}
+	
+
 	// This line closes the connection to the command line interface.
 	reader.close()
 	});
